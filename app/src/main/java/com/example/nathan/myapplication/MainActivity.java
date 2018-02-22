2c8379b6fdcd60ee42fa18a1837357841f174b2b
YoungChul Chun <youngcc3157@gmail.com>
2018-02-22 15:16:55 -0500

    Update on camera and gitignore

--- app/src/main/java/com/example/nathan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nathan/myapplication/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.hardware.Camera;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    //private Camera mCamera = null;
+    //private CheckersCamera mCameraView = null;
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -33,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(optionIntent);
             }
         });
+        /*
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CheckersCamera(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });*/
     }
 
     public void open(View view){
