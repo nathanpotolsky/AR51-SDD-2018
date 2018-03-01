4f99ae2d2c47f08f3145b34eef10f7964297af34
YoungChul Chun <youngcc3157@gmail.com>
2018-03-01 12:49:26 -0500

    Create Cameara view class

--- app/src/main/java/com/example/nathan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nathan/myapplication/MainActivity.java
@@ -40,27 +40,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(optionIntent);
             }
         });
-        /*
-        try{
-            mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e){
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-        }
-
-        if(mCamera != null) {
-            mCameraView = new CheckersCamera(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
-        }
-
-        //btn to close the application
-        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
-        imgClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });*/
     }
 
     public void open(View view){
