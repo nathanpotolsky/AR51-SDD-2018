f214ffc9b76bf832af8247b943534e2f4d0e9993
YoungChul Chun <youngcc3157@gmail.com>
2018-03-27 17:16:32 -0400

    Camera Filler page has been updated to take a photo from hardware

--- app/src/main/java/com/example/nathan/myapplication/CameraFillerPage.java
+++ app/src/main/java/com/example/nathan/myapplication/CameraFillerPage.java
@@ -1,25 +1,48 @@
 package com.example.nathan.myapplication;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraFillerPage extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_filler_page);
 
-        Button forwardButton = (Button)findViewById(R.id.forwardButton);
-        forwardButton.setOnClickListener(new View.OnClickListener(){
+        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Button forwardButton = (Button) findViewById(R.id.forwardButton);
+        forwardButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent optionIntent = new Intent(getApplicationContext(), ColorSelection.class);
                 startActivity(optionIntent);
             }
         });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 }
